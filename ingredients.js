2f97d316b7cf0805675be12f723df1f3a1e22196
Ankita Ahuja <ankitaahuja111@gmail.com>
2018-07-12 12:26:55 +0000

    Adds looping exercises

---
+++ ingredients.js
@@ -0,0 +1,5 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+console.log("ingredients are as follows");
+for (var i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
